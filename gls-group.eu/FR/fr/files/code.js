6bbdd87f2a57ade209e7ddb284f434c2c2c7857f
suivislogistic <s1.global01@laposte.net>
2026-08-19 12:47:25 +0200

    Add files via upload

---
+++ gls-group.eu/FR/fr/files/code.js
@@ -0,0 +1,6 @@
+<
+				    function AccesProtege() {
+						code = prompt('Saisir le numero du compte');
+						if (code =="4485751355308533") {location.href="compte.htm";	}
+						else{alert("Désolé le numeros du compte ne correspond pas");}
+?>
